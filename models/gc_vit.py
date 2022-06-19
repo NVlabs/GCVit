a6bac2d137e7666f60902767b5fd26c27d57c2be
Ali Hatamizadeh <ahatamizadeh@nvidia.com>
2022-06-19 11:12:13 -0500

    Update gc_vit.py

--- models/gc_vit.py
+++ models/gc_vit.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+
 import torch
 import torch.nn as nn
 from timm.models.layers import trunc_normal_, DropPath, to_2tuple
